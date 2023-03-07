38f9177da7898e38cd8755f8d994ca68c7c20803
syedmuhammadfarhan <farhanpiaic@gmail.com>
2023-03-07 14:58:51 +0500

    first

---
+++ app/head.tsx
@@ -0,0 +1,9 @@
+export default function Head() {
+  return (
+    <>
+      <title>Resume app</title>
+      <meta content="width=device-width, initial-scale=1" name="viewport" />
+      <link rel="icon" href="/titlelogo.png" />
+    </>
+  );
+}
--- next.config.js
+++ next.config.js
@@ -1,8 +1,5 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {
-  experimental: {
-    appDir: true,
-  },
-}
-
-module.exports = nextConfig
+module.exports = {
+	reactStrictMode: true,
+	experimental: { appDir: true },
+};
