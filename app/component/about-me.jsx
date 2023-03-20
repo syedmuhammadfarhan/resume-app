181b5bf4506412a98abf7bc10a1eff8b177155eb
syedmuhammadfarhan <farhanpiaic@gmail.com>
2023-03-20 09:21:04 +0500

    after aboutme mb

--- app/component/about-me.jsx
+++ app/component/about-me.jsx
@@ -6,7 +6,7 @@ const AboutMe = ({ data, skills }) => {
   const id = useId();
   return (
     <section>
-      <h2 className="mb-8"> {title}</h2>
+      <h2 className="mb-4"> {title}</h2>
       {body?.map((el, i) => (
         <p key={`${id}_${i}`} className="mb-6">
           {el}
